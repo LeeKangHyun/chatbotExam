4335eaaa88cebacb77922a1e983c7e8289c19771
DevKang <rkdgusdl92@gmail.com>
2018-06-16 00:00:23 +0900

    작업

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,82 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/keyboard', (req, res) => {
+  const data = {
+    'type': 'buttons',
+    'buttons': ["사진", "나이", "성별"]
+  };
+  
+  res.set({
+    'content-type': 'application/json'
+  }).json(data);
+});
+
+router.post('/message', (req, res) => {
+  const _obj = {
+    user_key: req.body.user_key,
+    type: req.body.type,
+    content: req.body.content
+  };
+  
+  let send = {
+    "message": {},
+    "keyboard": {
+      "type": "buttons",
+      "buttons": [
+        "사진",
+        "나이",
+        "성별",
+      ]
+    }
+  };
+  
+  switch(_obj.content) {
+    case '사진':
+      send['message']['text'] = '멍이 사진 투척';
+      send['message']['photo'] = {
+        'url': `/images/mung/mung-1.jpeg`,
+        "width": 640,
+        "height": 480
+      };
+      break;
+    case '나이':
+      break;
+    case '성별':
+      break;
+    default:
+      send['message']['text'] = '잘못된 선택입니다.';
+  }
+  
+  res.set({
+    'content-type': 'application/json'
+  }).send(JSON.stringify(send));
+});
+
+router.post('/friend', (req, res) => {
+  const user_key = req.body.user_key;
+  console.log(`${user_key}님이 쳇팅방에 참가했습니다.`);
+  
+  res.set({
+    'content-type': 'application/json'
+  }).send(JSON.stringify({success:true}));
+});
+
+router.delete('/friend', (req, res) => {
+  const user_key = req.body.user_key;
+  console.log(`${user_key}님이 쳇팅방을 차단했습니다.`);
+  
+  res.set({
+    'content-type': 'application/json'
+  }).send(JSON.stringify({success:true}));
+});
+
+router.delete('/chat_room/:user_key', (req, res) => {
+  const user_key = req.params.user_key;
+  console.log(`${user_key}님이 쳇팅방에서 나갔습니다.`);
+  res.set({
+    'content-type': 'application/json'
+  }).send(JSON.stringify({success:true}));
+});
+
 module.exports = router;
